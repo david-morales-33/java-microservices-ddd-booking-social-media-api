9a493cffbf88dd5772f6fd66492fd7c66f18bb4f
david-morales-33 <davidmorales33.97@gmail.com>
2026-04-06 16:12:26 -0500

    REF: OutboxEvent; <shared-kernel>

--- libs/shared-kernel/src/main/java/com/dmx/shared/kernel/events/OutboxEvent.java
+++ libs/shared-kernel/src/main/java/com/dmx/shared/kernel/events/OutboxEvent.java
@@ -1,6 +1,9 @@
 package com.dmx.shared.kernel.events;
 
+import com.dmx.shared.kernel.Utils;
+
 import java.sql.Timestamp;
+import java.time.Instant;
 
 public class OutboxEvent {
     private String id;
@@ -8,15 +11,23 @@ public class OutboxEvent {
     private String eventName;
     private String body;
     private Timestamp occurredOn;
-    private boolean published;
 
-    public OutboxEvent(String id, String aggregateId, String eventName, String body, Timestamp occurredOn, boolean published) {
+    public OutboxEvent(String id, String aggregateId, String eventName, String body, Timestamp occurredOn) {
         this.id = id;
         this.aggregateId = aggregateId;
         this.eventName = eventName;
         this.body = body;
         this.occurredOn = occurredOn;
-        this.published = published;
+    }
+
+    public static OutboxEvent fromDomainEvent(DomainEvent event) {
+        return new OutboxEvent(
+                event.eventId(),
+                event.aggregateId(),
+                event.eventName(),
+                Utils.jsonEncode(event.toPrimitives()),
+                Timestamp.from((Instant.parse((event.occurredOn()))))
+        );
     }
 
     protected OutboxEvent() {
@@ -42,9 +53,6 @@ public class OutboxEvent {
         return occurredOn;
     }
 
-    public boolean isPublished() {
-        return published;
-    }
 
     public void setId(String id) {
         this.id = id;
@@ -66,7 +74,4 @@ public class OutboxEvent {
         this.occurredOn = occurredOn;
     }
 
-    public void setPublished(boolean published) {
-        this.published = published;
-    }
 }
