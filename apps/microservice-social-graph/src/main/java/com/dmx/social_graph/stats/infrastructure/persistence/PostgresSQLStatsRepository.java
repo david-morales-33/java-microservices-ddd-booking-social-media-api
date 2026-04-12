ffe5977cd8b5e8a71ae9f9dc78e5ecae33205172
david-morales-33 <davidmorales33.97@gmail.com>
2026-04-12 18:01:42 -0500

    ADD: <PostgresSQLStatsRepository> ;<microservice-social-graph>

---
+++ apps/microservice-social-graph/src/main/java/com/dmx/social_graph/stats/infrastructure/persistence/PostgresSQLStatsRepository.java
@@ -0,0 +1,33 @@
+package com.dmx.social_graph.stats.infrastructure.persistence;
+
+import com.dmx.shared.kernel.Service;
+import com.dmx.social_graph.stats.domain.Stats;
+import com.dmx.social_graph.stats.domain.StatsRepository;
+import com.dmx.social_graph.stats.domain.UserId;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional("social_graph-transaction_manager")
+public class PostgresSQLStatsRepository implements StatsRepository {
+    protected final SessionFactory sessionFactory;
+
+    public PostgresSQLStatsRepository(@Qualifier("social_graph-session_factory") SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void save(Stats stats) {
+        sessionFactory.getCurrentSession().persist(stats);
+        sessionFactory.getCurrentSession().flush();
+        sessionFactory.getCurrentSession().clear();
+    }
+
+    @Override
+    public Optional<Stats> find(UserId id) {
+        return Optional.ofNullable(sessionFactory.getCurrentSession().byId(Stats.class).load(id));
+    }
+}
